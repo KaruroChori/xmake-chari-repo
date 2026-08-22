acf006b3baf28299482516b06c1d004c3103beab
Karuro Chari <karurochori@users.noreply.github.com>
2026-08-22 12:21:02 +0100

    Bump kd3 version.

--- packages/k/kd3/xmake.lua
+++ packages/k/kd3/xmake.lua
@@ -5,6 +5,7 @@ package("kd3")
 
     add_urls("https://github.com/karurochori/kd3.git")
 
+    add_versions("v1.1.0", "5bd769d76ab33a9cec46bcb5e90ece26f00f72dd")
     add_versions("v1.0.1", "86de5925e1cbfb36e93a3b5c080e5507e8a59ecb")
 
     on_install(function (package)

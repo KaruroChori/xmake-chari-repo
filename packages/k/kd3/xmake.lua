7bcefc706207a8bb91257585ed548295298e405f
KaruroChori <KaruroChori@users.noreply.github.com>
2026-09-01 15:57:14 +0100

    Bump kd3

--- packages/k/kd3/xmake.lua
+++ packages/k/kd3/xmake.lua
@@ -5,6 +5,7 @@ package("kd3")
 
     add_urls("https://github.com/karurochori/kd3.git")
 
+    add_versions("v1.3.0", "d482ca2fdbb3d6cf9c5908aff670384f4f3f466c")
     add_versions("v1.1.0", "5bd769d76ab33a9cec46bcb5e90ece26f00f72dd")
     add_versions("v1.0.1", "86de5925e1cbfb36e93a3b5c080e5507e8a59ecb")
 

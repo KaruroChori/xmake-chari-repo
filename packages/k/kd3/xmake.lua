5744719efe711c935e68176c40c1e2a27332b48f
Karuro Chari <karurochori@users.noreply.github.com>
2026-09-21 18:04:20 +0100

    kd3: use git.chatomari.work as primary source

--- packages/k/kd3/xmake.lua
+++ packages/k/kd3/xmake.lua
@@ -1,9 +1,10 @@
 package("kd3")
-    set_homepage("https://github.com/karurochori/kd3")
+    set_homepage("https://git.chatomari.work/lazy-eggplant/kd3")
     set_description("KD-Tree acceleration library")
     set_license("AGPL3")
 
-    add_urls("https://github.com/karurochori/kd3.git")
+    add_urls("https://git.chatomari.work/lazy-eggplant/kd3.git",
+             "https://github.com/karurochori/kd3.git")
 
     add_versions("v1.3.0", "d482ca2fdbb3d6cf9c5908aff670384f4f3f466c")
     add_versions("v1.1.0", "5bd769d76ab33a9cec46bcb5e90ece26f00f72dd")

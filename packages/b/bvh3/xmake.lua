2a6a868d2b992934258462eaa0e27bb3e152a5b7
Karuro Chari <karurochori@users.noreply.github.com>
2026-09-23 07:20:43 +0100

    add bvh3 (git.internal.chatomari.work primary source)

---
+++ packages/b/bvh3/xmake.lua
@@ -0,0 +1,18 @@
+package("bvh3")
+    set_homepage("https://git.internal.chatomari.work/lazy-eggplant/bvh3")
+    set_description("BVH acceleration library")
+    set_license("AGPL-3.0-only")
+
+    add_urls("https://git.internal.chatomari.work/lazy-eggplant/bvh3.git",
+             "https://github.com/karurochori/bvh3.git")
+
+    on_install(function (package)
+        import("package.tools.xmake").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <bvh3/bvh3.hpp>
+            void test() { /* use library API */ }
+        ]]}, {configs = {languages = "c++23"}}))
+    end)
